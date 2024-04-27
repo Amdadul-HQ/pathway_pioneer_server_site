cd622f1f6548bee78da9f03898ba7995cddee47d
AmdadulHQ <rimonamdadul301@gmail.com>
2024-04-27 23:49:30 +0600

    adding env to uri

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-// R7VTZvplvEpifys6
 
 
-const uri = "mongodb+srv://rimonamdadul301:R7VTZvplvEpifys6@cluster0.p2unx4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p2unx4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("pathwaypioneersDB");
     const countryCollection = database.collection("country");
@@ -99,8 +99,8 @@ async function run() {
           bordered_radio:updateSpot.updatebordered_radio,
           totalVisitorsPerYear:updateSpot.updatetotalVisitorsPerYear,
           photourl:updateSpot.updatephotourl,
-          travel_time:updateSpot.updateaverage_cost,
-          average_cost:updateSpot.updatetravel_time,
+          travel_time:updateSpot.updatetravel_time,
+          average_cost:updateSpot.updateaverage_cost,
           email:updateSpot.updateemail,
           userName:updateSpot.updateuserName
         }
@@ -118,11 +118,8 @@ async function run() {
       res.send(result)
     })
 
-
-
-
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
